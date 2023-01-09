7a65eefd011580f32f0e99f5869cd42a3eef52c2
Reclizer <cq1277202607@hotmail.com>
2023-01-09 23:38:55 +0800

    material

--- src/main/java/com/reclizer/inevo/item/weapon/ItemPhaseDevice.java
+++ src/main/java/com/reclizer/inevo/item/weapon/ItemPhaseDevice.java
@@ -1,5 +1,6 @@
 package com.reclizer.inevo.item.weapon;
 
+import com.reclizer.inevo.IndustrialEvolved;
 import com.reclizer.inevo.entity.construct.EntityFloatingCannon;
 import com.reclizer.inevo.item.ItemEnergyBase;
 
@@ -7,41 +8,85 @@ import com.reclizer.inevo.player.PlayerProperties;
 import com.reclizer.inevo.player.PlayerEnergy;
 import com.reclizer.inevo.tools.RayTraceTools;
 import com.reclizer.inevo.util.ParticleBuilder;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumHand;
-import net.minecraft.util.SoundCategory;
+import net.minecraft.util.*;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
 
 public class ItemPhaseDevice extends ItemEnergyBase {
+
     public ItemPhaseDevice(double maxElectricity, String name) {
         super(maxElectricity, name);
-        setFull3D();
+
+        this.addPropertyOverride(new ResourceLocation(IndustrialEvolved.MODID, "phase_device_state"), new IItemPropertyGetter() {
+            @Override
+            @SideOnly(Side.CLIENT)
+            public float apply(ItemStack stack, @Nullable World world, @Nullable EntityLivingBase entity)
+            {
+                boolean isHyperState = getHyperState(stack);
+                float hyperState = getHyperValue(stack);
+                if(isHyperState)
+                {
+                    if(hyperState <= 0.71f)
+                    {
+                        hyperState += 0.002f;
+                    }
+                    else
+                    {
+                        hyperState = 0.1f;
+                    }
+                    setHyperValue(stack, hyperState);
+                    return hyperState;
+                }
+                return 0.0f;
+            }
+        });
     }
+
+//    @Override
+//    public void onUpdate(ItemStack stack, World world, Entity entity, int something, boolean somethingelse) {
+//        if(!world.isRemote) {
+//            int meta = stack.getItemDamage();
+//            int newMeta = isActivated ? 1 : 0;
+//            if(meta != newMeta)
+//                stack.setItemDamage(newMeta);
+//        }
+//    }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack item = player.getHeldItem(hand);
         // ²¥·ÅÓÒ¼üµÄÉùÒô
         world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ENDERMEN_TELEPORT, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
-
-
-        if(!world.isRemote){
-            PlayerEnergy playerEnergy = PlayerProperties.getPlayerSummoned(player);
-
-            if(playerEnergy.getSpaceEnergy()>= 10){
-                playerEnergy.setSpaceEnergy(playerEnergy.getSpaceEnergy()-10);
-                EntityFloatingCannon floatingCannon = new EntityFloatingCannon(world);
-                floatingCannon.setPosition(player.posX, player.posY+3, player.posZ);
-                floatingCannon.setMaster(player);
-                world.spawnEntity(floatingCannon);
-            }
-
+        if(player.isSneaking()){
+            boolean isHyperState = getHyperState(item);
+            isHyperState = !isHyperState;
+            setHyperState(item, isHyperState);
         }
+
+
+//        if(!world.isRemote){
+//            PlayerEnergy playerEnergy = PlayerProperties.getPlayerSummoned(player);
+//
+//            if(playerEnergy.getSpaceEnergy()>= 10){
+//                playerEnergy.setSpaceEnergy(playerEnergy.getSpaceEnergy()-10);
+//                EntityFloatingCannon floatingCannon = new EntityFloatingCannon(world);
+//                floatingCannon.setPosition(player.posX, player.posY+3, player.posZ);
+//                floatingCannon.setMaster(player);
+//                world.spawnEntity(floatingCannon);
+//            }
+//
+//        }
 
 
 
@@ -101,6 +146,28 @@ public class ItemPhaseDevice extends ItemEnergyBase {
     public int getMaxItemUseDuration(ItemStack itemstack) {
         return 72000;
     }
+
+
+//=================================================================================================
+private void setHyperState(ItemStack stack, boolean state) {stack.getItem().getNBTShareTag(stack).setBoolean("isHyperState", state);}
+
+    private boolean getHyperState(ItemStack stack)
+    {
+        boolean value = false;
+        try {
+            value = stack.getItem().getNBTShareTag(stack).getBoolean("isHyperState");
+        } catch (Exception expt) {}
+        return value;
+    }
+
+    private void setHyperValue(ItemStack stack, float value) {stack.getItem().getNBTShareTag(stack).setFloat("HyperValue", value);}
+    private float getHyperValue(ItemStack stack)
+    {
+        float value = 0.0f;
+        try {
+            value = stack.getItem().getNBTShareTag(stack).getFloat("HyperValue");
+        } catch (Exception expt) {}
+        return value;
+    }
+
 }
-
-
