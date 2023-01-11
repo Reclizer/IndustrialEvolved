d1df7fa45cd75a7d2c143e705046730e9cc12e0a
Reclizer <cq1277202607@hotmail.com>
2023-01-11 12:45:36 +0800

    Update ItemPhaseDevice.java

--- src/main/java/com/reclizer/inevo/item/weapon/ItemPhaseDevice.java
+++ src/main/java/com/reclizer/inevo/item/weapon/ItemPhaseDevice.java
@@ -4,8 +4,10 @@ import com.reclizer.inevo.IndustrialEvolved;
 import com.reclizer.inevo.entity.construct.EntityFloatingCannon;
 import com.reclizer.inevo.item.ItemEnergyBase;
 
+import com.reclizer.inevo.item.ModItems;
 import com.reclizer.inevo.player.PlayerProperties;
 import com.reclizer.inevo.player.PlayerEnergy;
+import com.reclizer.inevo.potion.PotionRegistryHandler;
 import com.reclizer.inevo.tools.RayTraceTools;
 import com.reclizer.inevo.util.ParticleBuilder;
 import net.minecraft.entity.Entity;
@@ -14,6 +16,8 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.IItemPropertyGetter;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.*;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
@@ -24,7 +28,9 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import javax.annotation.Nullable;
 
 public class ItemPhaseDevice extends ItemEnergyBase {
-
+    private int chargeTime;
+    private final int maxCharge=400;
+    private  boolean portal;
     public ItemPhaseDevice(double maxElectricity, String name) {
         super(maxElectricity, name);
 
@@ -35,44 +41,126 @@ public class ItemPhaseDevice extends ItemEnergyBase {
             {
                 boolean isHyperState = getHyperState(stack);
                 float hyperState = getHyperValue(stack);
-                if(isHyperState)
-                {
-                    if(hyperState <= 0.71f)
-                    {
-                        hyperState += 0.002f;
-                    }
-                    else
-                    {
-                        hyperState = 0.1f;
-                    }
+                int chargeValue =getChargeValue(stack);
+
+                if(entity==null){
+                    return 0.0f;
+                }
+
+               if(isHyperState){
+                   //if(chargeValue%100==0) {
+                       int i = chargeValue<100 ? 0:chargeValue<200 ?1:chargeValue<300 ?2:chargeValue<400 ? 3:chargeValue<500 ?4:0;
+                       switch (i) {
+                           case 0:
+                               hyperState = 0.1f;
+                               break;
+                           case 1:
+                               hyperState = 0.2f;
+                               break;
+                           case 2:
+                               hyperState = 0.3f;
+                               break;
+                           case 3:
+                               hyperState = 0.4f;
+                               break;
+                           case 4:
+                               hyperState = 0.5f;
+                               break;
+                       }
+                   //}
                     setHyperValue(stack, hyperState);
                     return hyperState;
                 }
+
+
+
                 return 0.0f;
             }
         });
     }
+    @Override
+    public void onUpdate(ItemStack stack, World world, Entity entity, int something, boolean somethingelse) {
+        if(!world.isRemote&&getHyperState(stack)) {
 
-//    @Override
-//    public void onUpdate(ItemStack stack, World world, Entity entity, int something, boolean somethingelse) {
-//        if(!world.isRemote) {
-//            int meta = stack.getItemDamage();
-//            int newMeta = isActivated ? 1 : 0;
-//            if(meta != newMeta)
-//                stack.setItemDamage(newMeta);
+            int chargeValue =getChargeValue(stack);
+            if(chargeValue<400){
+                setChargeValue(stack,chargeValue+1);
+            }else {
+                setPortalState(stack,true);
+            }
+        }
+    }
+
+//    private void updateAnimeState(ItemStack stack, Entity entity){
+//        if(stack.isEmpty()) return;
+//        if(!(stack.getItem() instanceof ItemPhaseDevice)) return;
+//
+//        NBTTagCompound tag = stack.getOrCreateSubCompound("chargeValue");
+//        boolean currentState = tag.getBoolean(HAS_SHIELD);
+//
+//        boolean updateState = false;
+//        if(entity instanceof EntityLivingBase){
+//            EntityLivingBase owner = (EntityLivingBase) entity;
+//            ItemStack shield = owner.getHeldItemOffhand();
+//            if(!shield.isEmpty()){
+//                if(shield.getItem().isShield(shield, owner))
+//                    updateState = true;
+//            }
 //        }
 //    }
+    @Override
+    public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged)
+    {
+        if(getHyperState(newStack)){
+            return false;
+        }else {
+            return true;
+        }
+        //return !oldStack.equals(newStack); //!ItemStack.areItemStacksEqual(oldStack, newStack);
+    }
 
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
         ItemStack item = player.getHeldItem(hand);
         // ²¥·ÅÓÒ¼üµÄÉùÒô
         world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_ENDERMEN_TELEPORT, SoundCategory.NEUTRAL, 0.5F, 0.4F / (itemRand.nextFloat() * 0.4F + 0.8F));
+
+
+
+
         if(player.isSneaking()){
             boolean isHyperState = getHyperState(item);
             isHyperState = !isHyperState;
             setHyperState(item, isHyperState);
+            return new ActionResult<>(EnumActionResult.FAIL, item);
         }
+
+
+        if (!world.isRemote) {
+            if(!getHyperState(item)){
+                if(getEnergy(item) > 0){
+                    player.addPotionEffect(new PotionEffect(PotionRegistryHandler.PHASE_POTION_II, 25, 0));
+                    if (!player.capabilities.isCreativeMode) {
+                        setEnergy(item, getEnergy(item) - 1000);
+                    }
+                    player.getCooldownTracker().setCooldown(this, 60);
+                }
+            }else {
+                if(getPortalState(item)){
+                    player.addPotionEffect(new PotionEffect(PotionRegistryHandler.PHASE_POTION_II, 100, 0));
+                    //player.getCooldownTracker().setCooldown(this, 600);
+                    setPortalState(item,false);
+                    setChargeValue(item,0);
+                    setHyperValue(item,0.1f);
+                    return new ActionResult<>(EnumActionResult.FAIL, item);
+                }
+            }
+
+        }
+
+
+
+
 
 
 //        if(!world.isRemote){
@@ -125,19 +213,8 @@ public class ItemPhaseDevice extends ItemEnergyBase {
 
 
 
-//        if (!world.isRemote) {
-//            if(getEnergy(item) > 0){
-//                player.addPotionEffect(new PotionEffect(PotionRegistryHandler.PHASE_POTION_II, 25, 0));
-//                if (!player.capabilities.isCreativeMode) {
-//                    setEnergy(item, getEnergy(item) - 1000);
-//                }
-//
-//                player.getCooldownTracker().setCooldown(this, 60);
-//            }
-//
-//
-//        }
-        return new ActionResult<>(EnumActionResult.SUCCESS, item);
+
+        return new ActionResult<>(EnumActionResult.FAIL, item);
     }
 
 
@@ -149,7 +226,7 @@ public class ItemPhaseDevice extends ItemEnergyBase {
 
 
 //=================================================================================================
-private void setHyperState(ItemStack stack, boolean state) {stack.getItem().getNBTShareTag(stack).setBoolean("isHyperState", state);}
+    private void setHyperState(ItemStack stack, boolean state) {stack.getItem().getNBTShareTag(stack).setBoolean("isHyperState", state);}
 
     private boolean getHyperState(ItemStack stack)
     {
@@ -166,6 +243,27 @@ private void setHyperState(ItemStack stack, boolean state) {stack.getItem().getN
         float value = 0.0f;
         try {
             value = stack.getItem().getNBTShareTag(stack).getFloat("HyperValue");
+        } catch (Exception expt) {}
+        return value;
+    }
+
+    private void setPortalState(ItemStack stack, boolean state) {stack.getItem().getNBTShareTag(stack).setBoolean("isPortalState", state);}
+
+    private boolean getPortalState(ItemStack stack)
+    {
+        boolean value = false;
+        try {
+            value = stack.getItem().getNBTShareTag(stack).getBoolean("isPortalState");
+        } catch (Exception expt) {}
+        return value;
+    }
+
+    private void setChargeValue(ItemStack stack, int value) {stack.getItem().getNBTShareTag(stack).setInteger("chargeValue", value);}
+    private int getChargeValue(ItemStack stack)
+    {
+        int value = 0;
+        try {
+            value = stack.getItem().getNBTShareTag(stack).getInteger("chargeValue");
         } catch (Exception expt) {}
         return value;
     }
